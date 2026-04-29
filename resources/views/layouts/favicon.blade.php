6b6fc1a4c31857ff21d009f92143e194bbf0fb9e
Crossnexus <crossnexus.infra@gmail.com>
2026-04-30 07:07:01 +0900

    use favicon-16 globally across all views

--- resources/views/layouts/favicon.blade.php
+++ resources/views/layouts/favicon.blade.php
@@ -1,2 +1,3 @@
-<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16.png') }}?v=2">
-<link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon-16.png') }}?v=2">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicon-16.png') }}?v=3">
+<link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon-16.png') }}?v=3">
+<link rel="apple-touch-icon" href="{{ asset('images/favicon-16.png') }}?v=3">
